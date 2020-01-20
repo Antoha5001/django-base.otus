bd312a4c48cb9b1d7bc2509e146ac0bc1d88ecb9
Antoha5001 <plantsvzzombi@mail.ru>
2020-01-20 19:32:13 +0700

    '123'

--- myproject/homepage/urls.py
+++ myproject/homepage/urls.py
@@ -1,10 +1,24 @@
-from django.urls import path
+from django.urls import path, register_converter
+from django.views.generic.base import TemplateView
 
-from  .views import home_view, article_view
+from .views import article_view, article_view_year, HomeView, ArticleView
 
 app_name = "homepage"
 
+class YearConverter():
+    regex = r'[0-9]{4}'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return '%04d' % value
+
+register_converter(YearConverter, 'year')
+
 urlpatterns = [
-    path('', home_view, name="index"),
-    path('articles/', article_view, name="articles")
+    path('', TemplateView.as_view(template_name='homepage/index.html'), name="index"),
+    path('articles/', ArticleView.as_view(), name="articles"),
+    path('articles/<year:year>/', article_view_year, name='articles_year')
+    # re_path(r'^articles/(?P<year>[0-9]{4})/$', article_view_year, name='articles_year')
 ]
--- myproject/homepage/views.py
+++ myproject/homepage/views.py
@@ -1,34 +1,57 @@
+from random import randint
+from datetime import date, datetime, timedelta
+from django.views.generic import TemplateView
+
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
 
-require_http_methods(['POST', 'GET'])
-def home_view(request):
-    # response = HttpResponse("Hello")
-    # response['My-Custom-Header'] = 'meat-and-eggs'
+def today(count=20):
+    days = []
+    today = datetime.today() - timedelta(days=8)
+    for i in range(count):
+        print(i)
+        day = today - timedelta(days=i)
+        # print(day)
+        for j in range(randint(2, 5)):
+            print(str(i) + '-' + str(j))
+            days.append(day.replace(hour=randint(0, 23)))
+    return days
+    # print(len(days))
 
-    if request.method == 'GET':
-        print('This is GET')
-    response = render(request, 'homepage/index.html')
-    return response
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'homepage/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name=self.template_name)
+
+class ArticleView(TemplateView):
+    template_name = 'homepage/articles.html'
+
+    def get(self, request, *args, **kwargs):
+        myclass = MyClass()
+        myclass.data = {
+            'datakey': 'datavalue'
+        }
+        myclass.list = list(range(4, 8))
+        args = {
+            'articles': list(range(1, 4)),
+            'title0': '',
+            'title1': '<h3>title h3 1</h3>',
+            'title2': '<h4>title h4 2</h4>',
+            'myclass': myclass,
+            'dates_my': today()
+        }
+        today()
+        response = render(request, self.template_name, args)
+        return response
+
 
 def article_view(request):
-    myclass = MyClass()
-    myclass.data = {
-        'datakey': 'datavalue'
-    }
-    myclass.list = list(range(4,8))
-    args = {
-        'articles': list(range(1, 4)),
-        'title0': '',
-        'title1': '<h3>title h3 1</h3>',
-        'title2': '<h4>title h4 2</h4>',
-        'myclass': myclass
-    }
+    pass
 
-    response = render(request, 'homepage/articles.html', args)
-    return response
+def article_view_year(request, year):
+    return HttpResponse(f'<h1>{year}</h1>')
 
 class MyClass():
     foo = "foooooo"
