a9ccad3507ee1fde3949d3ac4977c569f05b10b2
Antoha5001 <plantsvzzombi@mail.ru>
2020-01-24 00:49:15 +0700

    123

--- myproject/authors/views.py
+++ myproject/authors/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import render, reverse, redirect
 from django.views.generic import TemplateView, ListView
 
 from .models import Author
+from .forms import AuthorForms
 
 class IndexPage(ListView):
     model = Author
@@ -9,8 +11,21 @@ class IndexPage(ListView):
 
 class CreatePage(TemplateView):
     template_name = 'authors/create-page.html'
+
     def get_context_data(self, **kwargs):
-        data = super().get_context_data()
+        data = super().get_context_data(**kwargs)
+        data['form'] = AuthorForms()
         return data
+
+    def post(self, request):
+        form = AuthorForms(request.POST)
+        if form.is_valid():
+            new_author = form.save(commit=False)
+            new_author.email = 'default@email.ru'
+            new_author.save()
+            return redirect(reverse('authors:authors'))
+        return render(request, self.template_name, {'form' : form})
+
+
 
 # Create your views here.
