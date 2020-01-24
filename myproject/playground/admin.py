c25523395a98735b36d0dd0b521b0287c48e2203
Antoha5001 <plantsvzzombi@mail.ru>
2020-01-24 19:34:07 +0700

    '123'

--- myproject/playground/admin.py
+++ myproject/playground/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Publisher,Articles, Student, StudentInfo
+
+admin.site.register([Publisher, Articles, Student, StudentInfo])
 
 # Register your models here.
--- myproject/playground/models.py
+++ myproject/playground/models.py
@@ -10,6 +10,72 @@ class StudentInformation(models.Model):
     def __str__(self):
         return f"{self.firsr_name} {self.last_name}"
 
+class Student(models.Model):
+
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class StudentInfo(models.Model):
+
+    pass_id = models.CharField(max_length=10, unique=True)
+    email = models.EmailField()
+    student = models.OneToOneField(
+        Student,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='info'
+    )
+    def __str__(self):
+        return f'{self.pass_id}'
+
+class Publisher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Publisher: {self.first_name} {self.last_name}'
+
+class Articles(models.Model):
+    title = models.CharField(max_length=250)
+    #body
+    date = models.DateField(auto_now=False)
+    publisher = models.ForeignKey(
+        Publisher,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.title}'
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    label = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.label}'
+
+class Shop(models.Model):
+
+    class Meta:
+        verbose_name = 'Shop'
+        verbose_name_plural = 'Shops'
+
+    title = models.CharField(max_length=100)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
 
 
 # Create your models here.
